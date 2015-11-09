4f96287ef516a72c8d348d78e7b5ceb3513cd552
Matt Owen <not.mattowen@gmail.com>
2015-11-08 19:09:30 -0500

    :wolf:

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
-var gulp = require('gulp'),
-    connect = require('gulp-connect');
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var browserify = require('gulp-browserify');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
+var react = require('gulp-react');
+
+gulp.task('jsx', function() {
+  return gulp.src('src/components/**.jsx')
+             .pipe(react())
+             .pipe(browserify())
+             .pipe(gulp.dest('app/components/'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/**.js')
+             .pipe(browserify({
+               insertGlobals : true
+             }))
+             .pipe(gulp.dest('./dist'));
+});
 
 gulp.task('connect', function() {
   connect.server({
@@ -7,5 +26,14 @@ gulp.task('connect', function() {
     livereload: true
   });
 });
+
+gulp.task('watch', function () {
+  watch('src/**/*.{js,jsx}', batch(function (events, done) {
+    gulp.run('jsx');
+    gulp.run('scripts');
+    done();
+  }));
+});
  
-gulp.task('default', ['connect']);
+// gulp.task('default', ['stream', 'connect']);
+gulp.task('default', ['watch', 'connect']);
